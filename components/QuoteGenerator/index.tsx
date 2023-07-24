aa169a7845a4089954e2614ceb4a203f2e15e864
RaiJashan17 <125523346+RaiJashan17@users.noreply.github.com>
2023-07-23 19:37:06 -0700

    Completed Features, Need to Debug Now

--- components/QuoteGenerator/index.tsx
+++ components/QuoteGenerator/index.tsx
@@ -97,7 +97,7 @@ const QuoteGeneratorModal = ({
                             </>
                         }
                         {/* State #2: Quote state fulfilled */}
-                        {quoteReceived === null &&
+                        {quoteReceived !== null &&
                             <>
                                 <QuoteGeneratorTitle>
                                     Download your quote!
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,17 @@ import QuoteGeneratorModal from '@/components/QuoteGenerator'
 import Cloud1 from '@/assets/cloud-and-thunder.png'
 import Cloud2 from '@/assets/cloudy-weather.png'
 import { API } from 'aws-amplify'
-import { quotesQueryName } from '@/src/graphql/queries'
+import { generateAQuote, quotesQueryName } from '@/src/graphql/queries'
 import { GraphQLResult } from '@aws-amplify/api-graphql'
 
+
+interface GenerateAQuoteData{
+  generateAQuote: {
+    statusCode: number;
+    headers: {[key:string]: string };
+    body: string;
+  }
+}
 // interface for DynamoDB object
 interface UpdateQuoteInfoData {
   id: string;
@@ -75,6 +83,8 @@ export default function Home() {
   // Functions for the quote generator modal
   const handleCloseGenerator = () => {
     setOpenGenerator(false);
+    setProcessingQuote(false);
+    setQuoteReceived(null);
   }
 
   const handleOpenGenerator = async (e: React.SyntheticEvent) => {
@@ -83,10 +93,28 @@ export default function Home() {
     setProcessingQuote(true);
     try {
       // Run Lambda Function
+      const runFunction="runFunction";
+      const runFunctionStringified= JSON.stringify(runFunction);
+      const response = await API.graphql<GenerateAQuoteData>({
+          query: generateAQuote,
+          authMode: "AWS_IAM",
+          variables: {
+            input: runFunctionStringified,
+          },
+      });
+      const responseStringified=JSON.stringify(response);
+      const responseReStringified=JSON.stringify(responseStringified);
+      const bodyIndex = responseReStringified.indexOf("body=") + 5;
+      const bodyAndBase64 = responseReStringified.substring(bodyIndex);
+      const bodyArray=bodyAndBase64.split(",");
+      const body=bodyArray[0];
+      console.log(body);
+      setQuoteReceived(body);
+
+      setProcessingQuote(false);
+
+      updateQuoteInfo();
       //setProcessingQuote(false);
-      setTimeout(() => {
-        setProcessingQuote(false);
-      },3000);
     } catch(error) {
       console.log('error generating quote:', error);
       setProcessingQuote(false);
